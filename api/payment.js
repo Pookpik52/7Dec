badc945a37e914b9c52574e3b321c14625f08de6
Pookpik52 <pookpiksharker@gmail.com>
2026-02-14 18:38:18 +0700

    limit tickets 80

--- api/payment.js
+++ api/payment.js
@@ -6,17 +6,27 @@ export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { name, email } = req.body;
 
-    try {
-      await sql`
-        INSERT INTO payments (name, email)
-        VALUES (${name}, ${email});
-      `;
+try {
 
-      return res.status(200).json({ message: 'Saved to database!' });
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ error: error.message });
-    }
+  const count = await sql`
+    SELECT COUNT(*) FROM payments WHERE status = 'approved';
+  `;
+
+  if (parseInt(count[0].count) >= 80) {
+    return res.status(400).json({ message: "Tickets sold out" });
+  }
+
+  await sql`
+    INSERT INTO payments (name, email)
+    VALUES (${name}, ${email});
+  `;
+
+  return res.status(200).json({ message: "Saved successfully" });
+
+} catch (error) {
+  return res.status(500).json({ error: error.message });
+}
+
   }
 
   return res.status(405).json({ message: 'Method not allowed' });

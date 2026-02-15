f20598f5fa1901a329e8361c643f7103328c2006
Pookpik52 <pookpiksharker@gmail.com>
2026-02-15 20:22:36 +0700

    reset to stable version

--- api/payment.js
+++ api/payment.js
@@ -3,32 +3,21 @@ import { neon } from '@neondatabase/serverless';
 const sql = neon(process.env.DATABASE_URL);
 
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
+
+  try {
     const { name, email } = req.body;
 
-try {
+    await sql`
+      INSERT INTO payments (name, email, status)
+      VALUES (${name}, ${email}, 'pending');
+    `;
 
-  const count = await sql`
-    SELECT COUNT(*) FROM payments WHERE status = 'approved';
-  `;
+    return res.status(200).json({ message: 'Saved successfully' });
 
-  if (parseInt(count[0].count) >= 80) {
-    return res.status(400).json({ message: "Tickets sold out" });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
   }
-
-await sql`
-  INSERT INTO payments (name, email, status)
-  VALUES (${name}, ${email}, 'pending');
-`;
-
-
-  return res.status(200).json({ message: "Saved successfully" });
-
-} catch (error) {
-  return res.status(500).json({ error: error.message });
-}
-
-  }
-
-  return res.status(405).json({ message: 'Method not allowed' });
 }
